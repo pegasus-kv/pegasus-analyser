f26511001c79f3aaaedf45a24d7d505eebaf8054
jiashuo <js982986555@live.com>
2022-02-18 19:21:36 +0800

    fix

--- src/main/scala/com/xiaomi/infra/pegasus/spark/analyser/PartitionIterator.scala
+++ src/main/scala/com/xiaomi/infra/pegasus/spark/analyser/PartitionIterator.scala
@@ -1,6 +1,5 @@
 package com.xiaomi.infra.pegasus.spark.analyser
 
-import com.xiaomi.infra.pegasus.tools.Tools
 import org.apache.commons.logging.LogFactory
 import org.apache.spark.TaskContext
 
@@ -17,30 +16,24 @@ private[analyser] class PartitionIterator private (
   private val LOG = LogFactory.getLog(classOf[PartitionIterator])
 
   private var pegasusScanner: PegasusScanner = _
-  private var filterExpiredRecord: Boolean = _
 
   private var closed = false
-  private var nextRecord =
-    PegasusRecord(
-      null,
-      null,
-      null,
-      0,
-      0
-    ) // the init value will be update by `next` and not be used
+  private var thisRecord: PegasusRecord = _
+  private var nextRecord: PegasusRecord = _
 
   private var name: String = _
-
-  private var expiredCount: Long = 0
-  private var totalCount: Long = 0
+  // TODO(wutao1): add metrics for counting the number of iterated records.
 
   def this(context: TaskContext, snapshotLoader: PegasusLoader, pid: Int) {
     this(context, pid)
 
     pegasusScanner = snapshotLoader.getScanner(pid)
-    filterExpiredRecord = snapshotLoader.getConfig.isFilterExpired
     pegasusScanner.seekToFirst()
     assert(pegasusScanner.isValid)
+    pegasusScanner.next() // skip the first record
+    if (pegasusScanner.isValid) {
+      nextRecord = pegasusScanner.restore()
+    }
     name = "PartitionIterator[pid=%d]".format(pid)
   }
 
@@ -49,35 +42,23 @@ private[analyser] class PartitionIterator private (
       // release the C++ pointers
       pegasusScanner.close()
       closed = true
-      LOG.info(
-        toString() + " closed, filter=" + filterExpiredRecord + ", total=" + totalCount + ", expired=" + expiredCount
-      )
+      LOG.info(toString() + " closed")
     }
   }
 
   override def hasNext: Boolean = {
-    updateNextRecord()
-    if (filterExpiredRecord) {
-      while (nextRecord != null && nextRecord.isExpired) {
-        expiredCount += 1
-        updateNextRecord()
-      }
-    }
     nextRecord != null && !closed
   }
 
   override def next(): PegasusRecord = {
-    nextRecord
-  }
-
-  def updateNextRecord(): Unit = {
+    thisRecord = nextRecord
     pegasusScanner.next()
     if (pegasusScanner.isValid) {
-      totalCount += 1
       nextRecord = pegasusScanner.restore()
     } else {
       nextRecord = null
     }
+    thisRecord
   }
 
   override def toString(): String = {
